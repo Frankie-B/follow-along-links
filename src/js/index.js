01cc60833b54cf908880f6e0389d83050ef4d04d
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-07-06 05:41:00 +0300

    Console mouseenter for highlightLink

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
 // jshint esversion: 6
 
 const triggers = document.querySelectorAll('a');
+
+function highlightLink() {
+
+    console.log(this);
+}
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
